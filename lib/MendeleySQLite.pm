de051492ef128435578ff04abe2768bb7b0099b0
Spiros Denaxas <s.denaxas@gmail.com>
2012-06-05 13:50:35 +0100

    Turned module into a Distzilla distribution

--- lib/MendeleySQLite.pm
+++ lib/MendeleySQLite.pm
@@ -7,6 +7,7 @@ package MendeleySQLite;
 
 use Data::Dumper;
 use DBI;
+use SQL::Abstract;
 
 =head2 new
 
@@ -29,6 +30,8 @@ sub new {
     unless ( $self->{dbh} ) {
         die "Could not connect to $rh_params->{dbfile}."
     }
+    
+    $self->{sql} = SQL::Abstract->new();
     
     return bless $self, $class;
 }
@@ -91,11 +94,94 @@ sub get_all_tags {
     
 }
 
+=head2 get_all_tags_for_document
+
+    Get all tags associated with a document in your library. Returns a reference to an array.
+    This method returns undef on error.
+    
+    my $ra_tags = $M->get_all_tags_for_document( $documentid );
+
+=cut
+
+sub get_all_tags_for_document {
+    my $self = shift;
+    my $document_id = shift;
+    
+    return undef if ( ! defined $document_id );
+    
+    my $ra_output = [ ];
+        
+    my ( $sql, $ra_bind ) = 
+        $self->_create_sql( 'DocumentTags', [ 'tag' ], { documentId => $document_id } );
+    
+    my $sth = $self->_execute_sql( $sql, $ra_bind );
+  
+    my $ra_all = $sth->fetchall_arrayref();
+    
+    unless ( scalar(@$ra_all) ) {
+        return $ra_output;
+    }    
+    
+    foreach my $ra ( @$ra_all ) {
+        push( @$ra_output, $ra->[0] );
+    }
+
+    return $ra_output;
+}
+
+=head2 get_all_keywords_for_document
+
+    Get all keywords associated with a document in your library. Returns a reference to an array.
+    This method returns undef on error.
+    
+    my $ra_keywords = $M->get_all_keywords_for_document( $documentid );
+
+=cut
+
+sub get_all_keywords_for_document {
+    my $self        = shift;
+    my $document_id = shift;
+    
+    return undef if ( ! defined $document_id );
+    
+    my $ra_output = [ ];
+        
+    my ( $sql, $ra_bind ) = 
+        $self->_create_sql( 'DocumentKeywords', [ 'keyword' ], { documentId => $document_id } );
+    
+    my $sth = $self->_execute_sql( $sql, $ra_bind );
+  
+    my $ra_all = $sth->fetchall_arrayref();
+    
+    unless ( scalar(@$ra_all) ) {
+        return $ra_output;
+    }    
+    
+    foreach my $ra ( @$ra_all ) {
+        push( @$ra_output, $ra->[0] );
+    }
+
+    return $ra_output;
+}
+
+sub _create_sql {
+    my $self      = shift;
+    my $table     = shift;
+    my $ra_fields = shift;
+    my $rh_params = shift;
+    
+    my ( $stmt, @bind ) =
+        $self->{sql}->select( $table, $ra_fields, $rh_params );
+        
+    return ( $stmt, \@bind );
+}
+
+
 sub _execute_sql {
     my $self = shift;
     my $sql  = shift;
     my $ra_params = shift || [ ];
-    
+        
     my $sth = 
         $self->{dbh}->prepare( $sql );
         
